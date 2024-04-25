20a34035d6ea1c272c1bb7e99814eb39cc8a0a7d
Bayan <b.nezamabad@gmail.com>
2024-04-25 20:15:14 +0100

    [BAYAN] Temporary disabling dark theme

--- app/src/main/java/com/example/cs4084_project/Login.java
+++ app/src/main/java/com/example/cs4084_project/Login.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -44,6 +45,8 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // temporary dark theme disable
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
