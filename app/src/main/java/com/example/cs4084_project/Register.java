6bedbb7b43d68ae32327a9b15ff97f82d06b15ba
Bayan <b.nezamabad@gmail.com>
2024-04-25 21:02:56 +0100

    [BAYAN] Added logging and fixed logic issue

--- app/src/main/java/com/example/cs4084_project/Register.java
+++ app/src/main/java/com/example/cs4084_project/Register.java
@@ -1,8 +1,11 @@
 package com.example.cs4084_project;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -19,11 +22,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -120,34 +125,35 @@ public class Register extends AppCompatActivity {
                 user.put("email", email);
                 user.put("first_name", firstName);
                 user.put("last_name", lastName);
-                final boolean[] success = {true};
                 db.collection("users")
                         .add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        progressBar.setVisibility(View.GONE);
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(Register.this, "Account registered successfully!", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(getApplicationContext(), Login.class);
+                                            startActivity(intent);
+                                            finish();
+                                        } else {
+                                            Toast.makeText(Register.this, "Authentication failed, please try again", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
+                        })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Toast.makeText(Register.this, "Failed to create account, please try again", Toast.LENGTH_SHORT).show();
+                                Log.w(TAG, "Failed to create account!", e);
                                 progressBar.setVisibility(View.GONE);
-                                success[0] = false;
                             }
                         });
-
-                if (success[0]) {
-                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            progressBar.setVisibility(View.GONE);
-                            if (task.isSuccessful()) {
-                                Toast.makeText(Register.this, "Account registered successfully!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(), Login.class);
-                                startActivity(intent);
-                                finish();
-                            } else {
-                                Toast.makeText(Register.this, "Authentication failed, please try again", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
             }
         });
     }
