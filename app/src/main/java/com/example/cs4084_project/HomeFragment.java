3d5a7465f29184f59b99818b9fcbfffa6dc713da
Bayan <b.nezamabad@gmail.com>
2024-05-01 21:57:05 +0100

    [BAYAN] Loading pfp from db

--- app/src/main/java/com/example/cs4084_project/HomeFragment.java
+++ app/src/main/java/com/example/cs4084_project/HomeFragment.java
@@ -1,19 +1,30 @@
 package com.example.cs4084_project;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.example.cs4084_project.classes.Post;
 import com.example.cs4084_project.classes.PostAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -21,6 +32,7 @@ public class HomeFragment extends Fragment {
 
     FirebaseAuth auth;
     FirebaseUser user;
+    FirebaseFirestore db;
     ListView postsListView;
     ArrayList<Post> posts = new ArrayList<>();
 
@@ -33,6 +45,7 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
 
         if (user == null) {
             Intent intent = new Intent(requireActivity().getApplicationContext(), Login.class);
@@ -40,13 +53,30 @@ public class HomeFragment extends Fragment {
             requireActivity().finish();
         }
 
-        Post p = new Post("a", "b", "c", "d", "e");
-        posts.add(p);
-
+        this.setProfilePictureImageView(rootView, user.getUid());
         postsListView = rootView.findViewById(R.id.posts);
         PostAdapter adapter = new PostAdapter(this.getContext(), posts);
         postsListView.setAdapter(adapter);
 
         return rootView;
+    }
+
+    private void setProfilePictureImageView(View view, String uid) {
+        ImageView profilePicture = view.findViewById(R.id.profile_picture);
+        DocumentReference userDoc = db.collection("users").document(uid);
+        userDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    String profilePic = task.getResult().getString("profile_picture");
+                    if (profilePic != null) {
+                        profilePicture.setImageTintList(null);
+                        Picasso.get().load(profilePic).into(profilePicture);
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
 }
