18b07c6aa678569af2c7fbbf6a9959196a8b4732
Euan Bourke <euanbourke@gmail.com>
2024-05-03 16:32:37 +0100

    profile: added user profile pic/username
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- app/src/main/java/com/example/cs4084_project/AccountFragment.java
+++ app/src/main/java/com/example/cs4084_project/AccountFragment.java
@@ -1,23 +1,36 @@
 package com.example.cs4084_project;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 public class AccountFragment extends Fragment {
 
     FirebaseAuth auth;
-    Button signOutButton;
     FirebaseUser user;
+    FirebaseFirestore db;
+    Button signOutButton;
 
     public AccountFragment() {
     }
@@ -29,12 +42,15 @@ public class AccountFragment extends Fragment {
         auth = FirebaseAuth.getInstance();
         signOutButton = rootView.findViewById(R.id.sign_out);
         user = auth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
 
         if (user == null) {
             Intent intent = new Intent(requireActivity().getApplicationContext(), Login.class);
             startActivity(intent);
             requireActivity().finish();
         }
+
+        fillUserInfo(rootView, user.getUid());
 
         signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +62,33 @@ public class AccountFragment extends Fragment {
             }
         });
 
+
+
         return rootView;
+    }
+
+    private void fillUserInfo(View view, String uid) {
+
+        ImageView profilePicture = view.findViewById(R.id.profile_picture);
+        TextView username = view.findViewById(R.id.profile_name);
+
+        DocumentReference userDoc = db.collection("users").document(uid);
+
+        userDoc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    String profilePic = task.getResult().getString("profilePicture");
+                    String name = task.getResult().getString("username");
+                    username.setText(name);
+                    if (profilePic != null) {
+                        profilePicture.setImageTintList(null);
+                        Picasso.get().load(profilePic).into(profilePicture);
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
     }
 }
