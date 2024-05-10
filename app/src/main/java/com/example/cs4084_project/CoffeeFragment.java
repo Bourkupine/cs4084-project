009c0ea3cc2cbd029748dd6df775ad2f41dfad05
Euan Bourke <euanbourke@gmail.com>
2024-05-10 14:35:40 +0100

    friends: inital commit for friends
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- app/src/main/java/com/example/cs4084_project/CoffeeFragment.java
+++ app/src/main/java/com/example/cs4084_project/CoffeeFragment.java
@@ -5,17 +5,29 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.example.cs4084_project.classes.Friend;
+import com.example.cs4084_project.classes.FriendAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-public class CoffeeFragment extends Fragment {
+import java.util.ArrayList;
+
+public class CoffeeFragment extends Fragment implements FriendAdapter.OpenFriend {
 
     FirebaseAuth auth;
     FirebaseUser user;
+    FirebaseFirestore db;
+    ArrayList<Friend> user_list = new ArrayList<>();
+    ListView list;
+    FriendAdapter adapter_friends;
 
     public CoffeeFragment() {
     }
@@ -26,6 +38,7 @@ public class CoffeeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_coffee, container, false);
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+        db = FirebaseFirestore.getInstance();
 
         if (user == null) {
             Intent intent = new Intent(requireActivity().getApplicationContext(), Login.class);
@@ -33,6 +46,34 @@ public class CoffeeFragment extends Fragment {
             requireActivity().finish();
         }
 
+        getAllUsers();
+
+        list = rootView.findViewById(R.id.userList);
+        adapter_friends = new FriendAdapter(requireContext(), user_list, this);
+        list.setAdapter(adapter_friends);
+
         return rootView;
+    }
+
+    void getAllUsers() {
+        db.collection("users").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Friend f = new Friend();
+                    f.setUid(document.getString("uid"));
+                    f.setUsername(document.getString("username"));
+                    f.setProfilePic(document.getString("profilePicture"));
+                    user_list.add(f);
+                    adapter_friends.notifyDataSetChanged();
+                }
+            } else {
+                Log.e("Firestore", "Error fetching friends: " + task.getException());
+            }
+        });
+    }
+
+    @Override
+    public void removeFriend(String friendId) {
+
     }
 }
